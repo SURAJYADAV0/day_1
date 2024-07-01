80505f833c756fa1b3ee0bc12b0a06790ab1c6db
SURAJYADAV0 <141991522+SURAJYADAV0@users.noreply.github.com>
2024-07-01 10:29:04 +0530

    Add files via upload

---
+++ 3_Add_Comments.py
@@ -0,0 +1,12 @@
+# Printing "Hello, World!" to the console
+print("Hello, World!")
+
+# Storing name, age, and favorite hobby in variables
+name = "John Doe"  # Name variable
+age = 30  # Age variable
+favorite_hobby = "reading"  # Favorite hobby variable
+
+# Printing the variables
+print("Name:", name)  # Print the name
+print("Age:", age)  # Print the age
+print("Favorite Hobby:", favorite_hobby)  # Print the favorite hobby
